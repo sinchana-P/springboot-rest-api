858de62eccf1e7160b5616b9130f7f791c69f374
sinchanap <sinchanap@ivoyant.com>
2025-02-09 21:01:00 +0530

    Spring Boot REST API That Returns List

--- src/main/java/net/sinch/springboot/controller/StudentController.java
+++ src/main/java/net/sinch/springboot/controller/StudentController.java
@@ -4,12 +4,27 @@ import net.sinch.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
-    @GetMapping("student")
+//    http://localhost:8080/student
+    @GetMapping("/student")
     public Student getStudent() {
         Student student = new Student(1, "Sinchana", "P Gudagi");
         return student;
+    }
+
+//    http://localhost:8080/students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Lily", "Daisy"));
+        students.add(new Student(2, "Rose", "Jack"));
+        students.add(new Student(3, "Marie", "Peter"));
+        students.add(new Student(4, "Jasmine", "Jaz"));
+        return students;
     }
 }

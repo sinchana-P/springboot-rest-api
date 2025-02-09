e3a69f6a8199ed558cc64f120e6d39d10baafa21
sinchanap <sinchanap@ivoyant.com>
2025-02-09 19:48:32 +0530

    HelloWorldController

---
+++ src/main/java/net/sinch/springboot/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package net.sinch.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // HTTP GET Request
+    // http://localhost:8080/hello-world
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}

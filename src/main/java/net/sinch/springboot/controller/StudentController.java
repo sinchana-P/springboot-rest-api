c189b0f128307268735caa4826e4dbadeeb858b2
sinchanap <sinchanap@ivoyant.com>
2025-03-15 20:19:26 +0530

    Spring Boot REST API with Request Param - @RequestParam

--- src/main/java/net/sinch/springboot/controller/StudentController.java
+++ src/main/java/net/sinch/springboot/controller/StudentController.java
@@ -3,6 +3,7 @@ package net.sinch.springboot.controller;
 import net.sinch.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,7 +12,7 @@ import java.util.List;
 @RestController
 public class StudentController {
 
-    //    Spring Boot REST API returns - Java Bean
+//    Spring Boot REST API returns - Java Bean
 //    http://localhost:8080/student
     @GetMapping("/student")
     public Student getStudent() {
@@ -20,7 +21,7 @@ public class StudentController {
         return new Student(1, "Sinchana", "P Gudagi");
     }
 
-    //    Spring Boot REST API That Returns - List
+//    Spring Boot REST API That Returns - List
 //    http://localhost:8080/students
     @GetMapping("/students")
     public List<Student> getStudents() {
@@ -32,7 +33,7 @@ public class StudentController {
         return students;
     }
 
-    //    Spring Boot REST API with Path Variable - @PathVariable
+//    Spring Boot REST API with Path Variable - @PathVariable
 //    {id} - URI template variable
 //    http://localhost:8080/students/1
     @GetMapping("/students/{id}/{first-name}/{last-name}")
@@ -41,6 +42,20 @@ public class StudentController {
 //    }
     public Student studentPathVariable(@PathVariable("id") int studentId, @PathVariable("first-name") String firstName, @PathVariable("last-name") String lastName) {
         return new Student(studentId, firstName, lastName);
+    }
+
+//    Spring Boot REST API with Request Param - @RequestParam
+//    http://localhost:8080/students/query?id=1
+    @GetMapping("/students/query")
+    public Student studentRequestParam(@RequestParam int id) {
+        return new Student(id, "new-user-firstName", "new-user-lastName");
+    }
+
+
+    //    http://localhost:8080/students/queries?id=1&firstName=Sinchana&lastName=P Gudagi
+    @GetMapping("/students/queries")
+    public Student studentRequestParams(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName) {
+        return new Student(id, firstName, lastName);
     }
 }
 

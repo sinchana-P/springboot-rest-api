9c27e27ab874940bc36ae40a1dc985cf982e98af
sinchanap <sinchanap@ivoyant.com>
2025-03-15 21:23:06 +0530

    Spring Boot POST REST API - @PostMapping and @RequestBody

--- src/main/java/net/sinch/springboot/controller/StudentController.java
+++ src/main/java/net/sinch/springboot/controller/StudentController.java
@@ -1,10 +1,8 @@
 package net.sinch.springboot.controller;
 
 import net.sinch.springboot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -56,6 +54,16 @@ public class StudentController {
     @GetMapping("/students/queries")
     public Student studentRequestParams(@RequestParam int id, @RequestParam String firstName, @RequestParam String lastName) {
         return new Student(id, firstName, lastName);
+    }
+
+//    Spring Boot POST REST API - @PostMapping and @RequestBody
+    @PostMapping("/student/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 }
 

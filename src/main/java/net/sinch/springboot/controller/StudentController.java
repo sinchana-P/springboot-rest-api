d8d3e37a7c9bb3213ed8a840239c7075c5d07541
sinchanap <sinchanap@ivoyant.com>
2025-03-15 19:56:51 +0530

    Revert "Spring Boot REST API with Path Variable - @PathVariable"
    This reverts commit 71962cbd4b9ee16fa4eaf05f45081e26307ce211.

--- src/main/java/net/sinch/springboot/controller/StudentController.java
+++ src/main/java/net/sinch/springboot/controller/StudentController.java
@@ -2,7 +2,6 @@ package net.sinch.springboot.controller;
 
 import net.sinch.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,16 +10,13 @@ import java.util.List;
 @RestController
 public class StudentController {
 
-//    Spring Boot REST API returns - Java Bean
 //    http://localhost:8080/student
     @GetMapping("/student")
     public Student getStudent() {
-//        Student student = new Student(1, "Sinchana", "P Gudagi");
-//        return student;
-        return new Student(1, "Sinchana", "P Gudagi");
+        Student student = new Student(1, "Sinchana", "P Gudagi");
+        return student;
     }
 
-//    Spring Boot REST API That Returns - List
 //    http://localhost:8080/students
     @GetMapping("/students")
     public List<Student> getStudents() {
@@ -31,19 +27,4 @@ public class StudentController {
         students.add(new Student(4, "Jasmine", "Jaz"));
         return students;
     }
-
-//    Spring Boot REST API with Path Variable - @PathVariable
-//    {id} - URI template variable
-//    http://localhost:8080/students/1
-    @GetMapping("/students/{id}/{first-name}/{last-name}")
-//    public Student studentPathVariable(@PathVariable int id) {
-//        return new Student(id, "new-user-firstName", "new-user-lastName");
-//    }
-    public Student studentPathVariable(@PathVariable("id") int studentId, @PathVariable("first-name") String firstName, @PathVariable("last-name") String lastName) {
-        return new Student(studentId, firstName, lastName);
-    }
-
-
 }
-
-
